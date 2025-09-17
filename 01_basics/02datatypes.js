4d8cc431e9511fdfb6c96ff3a77dd324300cc519
Umer <120053541+UMERAYUB8410@users.noreply.github.com>
2025-09-17 11:54:34 +0000

    datatypes and variables

---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,28 @@
+"use strict  "; // treat all js code as newer version
+ 
+// alert( 3 + 4) we are using node js not browser
+
+let age = 18; 
+let isloggedin = false
+let state;
+
+/*
+string => ""
+bigint
+bool => true/false
+null => standalone
+undefined =>
+    symbol => unique 
+*/
+
+
+// object react  
+// null is alaways object if asked of what type is null
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+console.log(typeof 123n); //bigint
+console.log(typeof true); //boolean
+console.log(typeof "hello"); //string
+console.log(typeof state); //undefined
+console.log(typeof age); //number
---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+const account_Id = 123456;
+// account_Id = 2; not allowed as it was constant
+// two types of assigining value to variable let var (use let only for scope )
+let   accountEmail = "umerayub8412@gmail.com"
+var accountPassword = "12345";
+accountCity = "Karachi" ;
+let accountState; // undefined
+
+
+accountEmail = "UA30629@gmail.com"
+accountPassword = "221227"
+accountCity = "Lahore"
+
+ 
+console.log(account_Id); // method 1 for printing on terminal
+// print in table form 
+console.table([account_Id, accountEmail, accountPassword, accountCity,accountState]); // method 2 for printing on terminal
