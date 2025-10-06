72cf15a07b333cc4f5651c2057702316212b4f92
Umer <120053541+UMERAYUB8410@users.noreply.github.com>
2025-10-06 06:37:23 +0000

    more about arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,32 @@
+let fruits = ['banana' , 'apple' , 'mango']
+let dairy = ['yogurt','milk','eggs']
+
+fruits.push(dairy)  // pushes on the same array [ 'banana', 'apple', 'mango', [ 'yogurt', 'milk', 'eggs' ] ]
+
+console.log(fruits); //[ 'banana', 'apple', 'mango', [ 'yogurt', 'milk', 'eggs' ] ]
+console.log(fruits[3][2]); //eggs
+
+//  trying with concat
+let results1 = fruits.concat(dairy)
+console.log(results1);  // returns a new array 
+
+// this method is preferred  due to limitations of calling method  in others 
+
+const results = [...fruits , ...dairy] // also known as spread
+console.log(results)
+
+const another_array = [1,2,3 , [4,5,6] , 7 , [8 , 9],[10,11]]
+
+const real_lifearray = another_array.flat(Infinity)
+console.log(real_lifearray)
+
+console.log(Array.isArray('Umer')) //false
+console.log(Array.from('umer'))// [ 'u', 'm', 'e', 'r' ]
+console.log(Array.from({name: 'umer'}));// []
+
+
+let score1 = 100
+let score2 = 200
+let score3 =  300
+
+console.log(Array.of(score1,score2,score3));
